9aea4a9c388df72cbd275cc89cc91ba2c1bb7ae6
JamesMokua <jamieedgar02@gmail.com>
2023-06-21 03:04:11 +0300

    Create new prompt

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -28,7 +28,7 @@ const Nav = () => {
           height={30}
           className='object-contain'
         />
-        <p className='logo_text'>Promptopia</p>
+        <p className='logo_text'>SharedPrompt</p>
       </Link>
 
       {/* Desktop Navigation */}
